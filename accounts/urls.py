50ec254dc82044ebc07ab62cbc30f98c7cba2235
Earvin Joven <vinnijoven@gmail.com>
2020-03-26 15:54:05 -1000

    First Commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+from django.conf import settings
+
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+]
